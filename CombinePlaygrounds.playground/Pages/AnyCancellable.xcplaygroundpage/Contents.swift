368804076f2d44e223a318fe2db18b16a412c9c5
Péter Szabolcs Nagy <nagypeterszabolcs@gmail.com>
2020-04-27 14:16:27 +0200

    Fix some typos

--- CombinePlaygrounds.playground/Pages/AnyCancellable.xcplaygroundpage/Contents.swift
+++ CombinePlaygrounds.playground/Pages/AnyCancellable.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 import Combine
 /*: [Previous](@previous)
 
- We have talked about type erasure for `Publsihers` now we need to talk about type erasure for `Subscribers`
+ We have talked about type erasure for `Publishers` now we need to talk about type erasure for `Subscribers`
 
  ## 4 AnyCancellable
 
@@ -16,12 +16,12 @@ var cancellables: Set<AnyCancellable> = []
     .sinkPrint()
     .store(in: &cancellables)
 /*:
- Now we are able to cancel the subscribtion of our sink (even though in this case our publisher will send a completion before we even have the opportunity to cancel the subscribtion). Actually all of the publishers we have created until now, complete their data stream before even returning the `AnyCancellable`.
+ Now we are able to cancel the subscription of our sink (even though in this case our publisher will send a completion before we even have the opportunity to cancel the subscription). Actually all of the publishers we have created until now, complete their data stream before even returning the `AnyCancellable`.
  >Calling `store(in:)` on the returned cancellable, enables to keep our declarative flow.
 
  To leverage the possibility of cancelling a subscriber we are going to need other publishers. (This is also the result of how `Sink` is implemented but we are going to stick with it for now).
 
- The publisher we currently use publishes a certain amount of values and immediatly after a completion. Next we will learn about `Subjects` another concept from **Combine** that enables us to publish in an imperative fashion.
+ The publisher we currently use publishes a certain amount of values immediately after a completion. Next we will learn about `Subjects` another concept from **Combine** that enables us to publish in an imperative fashion.
  */
 
 //: [Next](@next)
