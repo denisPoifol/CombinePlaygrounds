2693325d0137e14ce1804a0d0ce794484f0dceea
Denis Poifol <denis.poifol@gmail.com>
2020-08-09 10:24:28 +0200

    Write operator

--- CombinePlaygrounds.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ CombinePlaygrounds.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -50,6 +50,7 @@
     - [6. Implementing a simple publisher](MyJust)
     - [7. Creating a UIEvent publisher](UIControlPublisher)
     - [8. Memory management](MemoryManagement)
+    - [9. Implementing an operator](Operator)
  - IV Annex
     - [1. Native subject crash](NativeSubjectsCrash)
 
